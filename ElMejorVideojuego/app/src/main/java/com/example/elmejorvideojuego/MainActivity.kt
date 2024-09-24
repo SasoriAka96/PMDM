cad49a807eda086eff780ec0e72824b4a2e153fd
SasoriAka96 <nahivallejo46@gmail.com>
2024-09-24 16:14:09 +0200

    El mejor videojuego terminado

--- ElMejorVideojuego/app/src/main/java/com/example/elmejorvideojuego/MainActivity.kt
+++ ElMejorVideojuego/app/src/main/java/com/example/elmejorvideojuego/MainActivity.kt
@@ -1,15 +1,52 @@
 package com.example.elmejorvideojuego
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.CheckBox
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+        //Creamos un evento para cuando pulsemos un boton se cambie el texto de acuerdo al radio button seleccionado
+
+        findViewById<RadioGroup>(R.id.radioGroup).setOnCheckedChangeListener { _,
+                                                                               i ->
+
+            when (i) {
+
+                //Cuando el boton de fallout este seleccionado el textview elegido cambia a "Te gusta el Fallout"
+
+                R.id.radioButtonFallout -> findViewById<TextView>(R.id.textViewCual).text =
+                    (getString(R.string.te_gusta_el) + " " + (getString(R.string.fallout)))
+
+                R.id.radioButtonLol -> findViewById<TextView>(R.id.textViewCual).text =
+                    (getString(R.string.te_gusta_el) + " " + (getString(R.string.lol)))
+
+                R.id.radioButtonFortnite -> findViewById<TextView>(R.id.textViewCual).text =
+                    (getString(R.string.te_gusta_el) + " " + (getString(R.string.fortnite)))
+
+                R.id.radioButtonTf2 -> findViewById<TextView>(R.id.textViewCual).text =
+                    (getString(R.string.te_gusta_el) + " " + (getString(R.string.tf2)))
+            }
+
         }
+
+        //Comprobamos que cuando el checkbox de te gusta este seleccionado el textview elegido cambia a "Te gusta"
+
+        findViewById<CheckBox>(R.id.checkBoxTeGusta).setOnClickListener {
+
+            if (findViewById<CheckBox>(R.id.checkBoxTeGusta).isChecked) {
+                findViewById<TextView>(R.id.textViewCual).text = (getString(R.string.ckbx_mola))
+
+            } else {
+                findViewById<TextView>(R.id.textViewCual).text = (getString(R.string.ckbx_noMola))
+            }
+        }
+
     }
+}
