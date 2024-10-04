715e25d4d9b2939b7f046e3679e8c8c8c831b311
SasoriAka96 <nahivallejo46@gmail.com>
2024-10-04 19:48:46 +0200

    walter

--- LLamarAWalter/app/src/main/java/com/example/llamarawalter/MainActivity.kt
+++ LLamarAWalter/app/src/main/java/com/example/llamarawalter/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.llamarawalter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val textView: TextView = findViewById(R.id.textView2)
+        val imageButton: ImageButton = findViewById(R.id.imageButton3)
+        val imageButtonColgar: ImageButton = findViewById(R.id.imageButtonColgar)
+        val imageView: ImageView = findViewById(R.id.imageView)
+        imageButton.setOnClickListener {
+            imageView.setImageResource(R.drawable.walterwhite2)
+            textView.setText(R.string.llamando)
+            imageButton.visibility = ImageButton.INVISIBLE
+            imageButtonColgar.visibility = ImageButton.VISIBLE
+        }
+        imageButtonColgar.setOnClickListener {
+            imageView.setImageResource(R.drawable.walterwhite)
+            textView.setText(R.string.llamada_terminada)
+            imageButton.visibility = ImageButton.VISIBLE
+            imageButtonColgar.visibility = ImageButton.INVISIBLE
+        }
     }
 }
